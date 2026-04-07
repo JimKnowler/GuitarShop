823289be23a65a565fa2cf1f1058ff1b5fa0871e
Roland Eschenburg <roland.eschenburg@hotmail.co.uk>
2026-04-07 21:45:30 +0200

    new item is added to the order’s items list with that product and sale quantity

--- guitarshop/guitarshop_test.py
+++ guitarshop/guitarshop_test.py
@@ -1,6 +1,27 @@
 import unittest
 
+class Order():
+    def __init__(self):
+        self.items = []
+    def add_item(self, product, quantity):
+        self.items.append((product, quantity))
+
+    def get_items(self):
+        return self.items
+
+class Product():
+    def __init__(self, id, stock, hold):
+        self.id = id
+        self.stock = stock
+        self.hold = hold
+
 class TestClass(unittest.TestCase):
     def test_basic(self):
         self.assertEqual(1, 1)
 
+    def test_new_item_added_to_order(self):
+        order = Order()
+        product = Product(327,7,0)
+        order.add_item(product, 1)
+        order_items = order.get_items()
+        self.assertEqual(order_items,[(product,1)])
