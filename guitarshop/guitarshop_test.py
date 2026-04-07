0e6549940e283d304cd08a4280782ac29d0e07ad
Jim Knowler <jim.knowler2@gmail.com>
2026-04-07 21:02:00 +0100

    separate file for Product  class

--- guitarshop/guitarshop_test.py
+++ guitarshop/guitarshop_test.py
@@ -1,5 +1,7 @@
 import unittest
 
+from product import Product
+ 
 class Order():
     def __init__(self):
         self.items = []
@@ -9,12 +11,6 @@ class Order():
 
     def get_items(self):
         return self.items
-
-class Product():
-    def __init__(self, id, stock, hold):
-        self.id = id
-        self.stock = stock
-        self.hold = hold
 
 class TestClass(unittest.TestCase):
     def test_new_item_added_to_order(self):
